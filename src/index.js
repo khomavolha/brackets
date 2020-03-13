31f9b7beede38a9cbd095880499714233d7abe84
Khoma Olya <52779547+khomavolha@users.noreply.github.com>
2020-03-13 21:45:38 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let br;
+  br = bracketsConfig.map(function(a){ return a.join("") });
+  for (let i = 0; i < br.length; i++) {
+    if (str.includes(br[i])) {
+      str = str.replace(br[i], "");
+    i=-1;}
+  }
+  if(str.length == 0) {return true}else{return false}
 }
